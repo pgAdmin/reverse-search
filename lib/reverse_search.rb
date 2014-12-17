1d510a0ddb6c6233e24d707280a751b6cbd1cba3
pgAdmin <punit.g7@gmail.com>
2014-12-17 10:57:10 +0530

    Support for Mac added

--- lib/reverse_search.rb
+++ lib/reverse_search.rb
@@ -1,7 +1,10 @@
 module Reverse
 	class Search
-		def self.find(regxp) 
-			file = File.open("/home/"+ENV['USER']+"/.bash_history", "r")
+		def self.find(regxp)
+			path = ""
+			path = "/home/"+ENV['USER']+"/.bash_history" if `uname -s`.gsub("\n", "")=="Linux"  
+			path = "/Users/"+ENV['USER']+"/.bash_history" if `uname -s`.gsub("\n", "")=="Darwin"  
+			file = File.open(path, "r")
 			all_lines = file.readlines
 			regexp = Regexp.new regxp
 			puts all_lines.select {|line| line.gsub("\n", "") =~ (regexp)}
